340307db02c3a7340cb927a7a984619fd353ba6f
rodgerwatercompany1 <rebornyschen@gmail.com>
2015-05-20 18:15:06 +0800

    Build
    http://blog.chinaunix.net/uid-26000296-id-3385904.html
    已在 rtmpdump.obj 中定義過了

---
+++ Project/stdafx.cpp
@@ -0,0 +1,9 @@
+// stdafx.cpp : 僅包含標準 Include 檔的原始程式檔
+// Project.pch 會成為先行編譯標頭檔
+// stdafx.obj 會包含先行編譯類型資訊
+
+#include "stdafx.h"
+
+#define RTMPDUMP_VERSION = 2.3
+// TODO: 在 STDAFX.H 中參考您需要的任何其他標頭，
+// 而不要在這個檔案中參考
